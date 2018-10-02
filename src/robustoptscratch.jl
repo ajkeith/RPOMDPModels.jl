83b9ce10727b3f1d1a4b0744bdc84445253410e2
ajkeith <keith.andrew.j@gmail.com>
2018-10-02 16:37:52 -0400

    correctness experiments

--- src/robustoptscratch.jl
+++ src/robustoptscratch.jl
@@ -167,13 +167,20 @@ Plots.plot([0,1], pol3.alphas, xticks = 0:0.05:1, lab = pol3.action_map, legend 
 # sim value agrees for tigerpomdp discount = 0.95 is about 19.5
 
 
+
+###############################
+#
+# Correctness testing
+#
+# TODO: Add to actual test file (i.e. take constant values from POMDP results and use that to test against in the runtests of RobustValueIteration)
+################################
 # compare RPOMDP to POMDP
 using RPOMDPs, RPOMDPModels, RPOMDPToolbox
 using RobustValueIteration
 using Plots; gr()
 
 prob = TigerPOMDP(0.95)
-prob2 = TigerRPOMDP(0.95, 0.1)
+prob2 = TigerRPOMDP(0.95, 0.01)
 sr = RPBVISolver(max_iterations = 10_000)
 sr2 = RPBVISolver(max_iterations = 1000)
 polr = RobustValueIteration.solve(sr, prob)
@@ -186,15 +193,52 @@ vr = discounted_reward(hr)
 vr2 = discounted_reward(hr2)
 value(polr, [0.5,0.5])
 value(polr2, [0.5,0.5])
-N = 10_000
+N = 1_000
 N2 = 1
-mean(discounted_reward(simulate(HistoryRecorder(max_steps=10_000), prob, polr, bur)) for i = 1:N)
-mean(discounted_reward(simulate(HistoryRecorder(max_steps=1000), prob2, polr2, bur2)) for i = 1:N2)
+mean(discounted_reward(simulate(HistoryRecorder(max_steps=1_000),
+                                                prob, polr, bur)) for i = 1:N)
+mean(discounted_reward(simulate(HistoryRecorder(max_steps=100),
+                                                prob2, polr2, bur2)) for i = 1:N2)
 
 # Robust point-based value iteration
 # POMDP
-Plots.plot([0,1], polr.alphas, xticks = 0:0.05:1, lab = polr.action_map, legend = :bottomright)
+Plots.plot([0,1], polr.alphas, xticks = 0:0.05:1,
+            lab = polr.action_map, legend = :bottomright)
 
 # Robust point-based value iteration
 # RPOMDP
-Plots.plot([0,1], polr2.alphas, xticks = 0:0.05:1, lab = polr2.action_map, legend = :bottomright)
+Plots.plot([0,1], polr2.alphas, xticks = 0:0.05:1,
+            lab = polr2.action_map, legend = :bottomright)
+
+
+prob = BabyPOMDP()
+sr = RPBVISolver(max_iterations = 10_000)
+polr = RobustValueIteration.solve(sr, prob)
+bur = updater(polr)
+hr = simulate(HistoryRecorder(max_steps=100), prob, polr, bur)
+vr = discounted_reward(hr)
+value(polr, [0.0, 1.0])
+N = 1_000
+mean(discounted_reward(simulate(HistoryRecorder(max_steps=1_000),
+                                                prob, polr, bur)) for i = 1:N)
+
+prob2 = BabyRPOMDP(0.2)
+sr2 = RPBVISolver(max_iterations = 1000)
+polr2 = RobustValueIteration.solve(sr2, prob2)
+bur2 = updater(polr2)
+hr2 = simulate(HistoryRecorder(max_steps=100), prob2, polr2, bur2)
+vr2 = discounted_reward(hr2)
+value(polr2, [0.0, 1.0])
+N2 = 1
+# mean(discounted_reward(simulate(HistoryRecorder(max_steps=100),
+                                            # prob2, polr2, bur2)) for i = 1:N2)
+
+# Robust point-based value iteration
+# POMDP
+Plots.plot([0,1], polr.alphas, xticks = 0:0.1:1,
+        lab = polr.action_map, legend = :bottomright)
+
+# Robust point-based value iteration
+# RPOMDP
+Plots.plot([0,1], polr2.alphas, xticks = 0:0.1:1,
+        lab = polr2.action_map, legend = :bottomright)
